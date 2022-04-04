cc119d7f372de9c4733bf3e504b457f5a23b17ee
nizar0rchid <nizar.ferchichi@esprit.tn>
2022-04-04 01:52:33 +0100

    Added square in app payment and check if a user is already subscribed to course

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
+
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:learnifyflutter/Courses%20Screens/addLessons.dart';
 import 'package:learnifyflutter/Welcome%20Screens/splashscreen.dart';
@@ -16,7 +16,7 @@ import 'package:learnifyflutter/test.dart';
 import 'chewie_list_item.dart';
 
 void main() {
-  Stripe.publishableKey = Stripekey;
+
   runApp(OverlaySupport.global(
     child: MaterialApp(
       home: MyApp(),
--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -1,3 +1,3 @@
-const String BaseURL = "http://172.20.10.3:3000/";
+const String BaseURL = "http://192.168.137.218:3000/";
 const String Stripekey =
     "pk_test_51KhxhoDsrNnTT1wSBPzFNXMddxF8Leb67HmdK5dtpiQP2lx5fYI2Zoat4xIA1HeUyhletIU4Uq99zXpJQW21C9EN00eINa4yXQ";
