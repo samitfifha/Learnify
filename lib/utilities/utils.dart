855a40a21538fbb6f1fa5119cdd81c22f0861c36
nizar0rchid <nizar.ferchichi@esprit.tn>
2022-04-23 23:49:27 +0100

    implemented the Ai recognizing expressions in a video meet, Preparing for merge

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -1,4 +1,4 @@
-const String BaseURL = "http://192.168.1.19:3000/";
+const String BaseURL = "http://172.17.0.233:3000/";
 
 const String Stripekey =
     "pk_test_51KhxhoDsrNnTT1wSBPzFNXMddxF8Leb67HmdK5dtpiQP2lx5fYI2Zoat4xIA1HeUyhletIU4Uq99zXpJQW21C9EN00eINa4yXQ";
